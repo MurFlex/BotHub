11c5593bfd8860ca36dff1899199210055c925fe
Kirill Mahonin <murflexmur@yandex.ru>
2024-09-24 23:07:47 +0300

    loader bh-8

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,15 @@
 import '@/styles/global.scss'
+import Button from './components/Button/Button'
+import Loader from './components/Loader/Loader'
 
 function App() {
-	return <div className='test'>test</div>
+	return (
+		<div className='test'>
+			<Button appearance='accent'>
+				<Loader />
+			</Button>
+		</div>
+	)
 }
 
 export default App
